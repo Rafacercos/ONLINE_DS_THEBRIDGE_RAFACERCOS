622df0034910626cf30ff2473970b08edbb8858b
Rafacercos <rafacercosmatas@gmail.com>
2025-03-13 20:07:29 +0100

    crisis superada

--- MIS_EJERCICIOS/utils.py
+++ MIS_EJERCICIOS/utils.py
@@ -21,9 +21,14 @@ def disparar(casilla, tablero):
 def crear_barco_aleatorio(eslora):
     fila = random.randint(0,9)
     columna = random.randint(0,9)
-    barco_aleatorio = [(fila, columna)]
-
     orientacion = random.choice(["Horizontal", "Vertical"])
+    if orientacion == "Horizontal":
+        columna = random.randint (0,9 - eslora)
+        fila = random.randint(0,9)
+    elif orientacion == "Vertical" :
+        fila = random.randint (0,9 - eslora)
+        columna = random.randint(0,9)
+    barco_aleatorio = [(fila, columna)]
     while len(barco_aleatorio) < eslora:
         if orientacion == "Horizontal":
             columna = columna + 1
@@ -32,3 +37,15 @@ def crear_barco_aleatorio(eslora):
             fila = fila + 1
             barco_aleatorio.append((fila, columna))
     return barco_aleatorio
+
+def crear_lista_barcos(lista_esloras=[2,2,2,3,3,4]):
+    lista_barcos = []
+    for i in lista_esloras:
+        lista_barcos.append(crear_barco_aleatorio(i))
+    return lista_barcos
+
+def colocar_barcos_random(tablero):
+    lista_barcos=crear_lista_barcos()
+    for barco in lista_barcos:
+        colocar_barco(barco,tablero)
+    return print (tablero)
