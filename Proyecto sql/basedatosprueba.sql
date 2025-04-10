29d3fd4da94676c82ab29631cf8f77d05968a879
rafacercos <rafacercosmatas@gmail.com>
2025-04-10 19:03:18 +0200

    ej

--- Proyecto sql/basedatosprueba.sql
+++ Proyecto sql/basedatosprueba.sql
@@ -1,7 +1,14 @@
 -- Eliminar tablas si existen
-DROP TABLE IF EXISTS entries CASCADE;
-DROP TABLE IF EXISTS categories CASCADE;
-DROP TABLE IF EXISTS authors CASCADE;
+DROP TABLE IF EXISTS campus CASCADE;
+DROP TABLE IF EXISTS promocion CASCADE;
+DROP TABLE IF EXISTS vertical CASCADE;
+DROP TABLE IF EXISTS modalidad CASCADE;
+DROP TABLE IF EXISTS grupo CASCADE;
+DROP TABLE IF EXISTS profesores CASCADE;
+DROP TABLE IF EXISTS alumnos CASCADE;
+DROP TABLE IF EXISTS proyectos CASCADE;
+DROP TABLE IF EXISTS calificaciones CASCADE;
+
 
 -- Crear tabla campus
 CREATE TABLE campus (
@@ -33,7 +40,7 @@ CREATE TABLE grupo (
   id_promocion int,
   id_campus int,
   id_modalidad int,
-  id_vertical int
+  id_vertical int,
   FOREIGN KEY (id_promocion) REFERENCES promocion(id_promocion),
   FOREIGN KEY (id_campus) REFERENCES campus(id_campus),
   FOREIGN KEY (id_modalidad) REFERENCES modalidad(id_modalidad),
@@ -45,7 +52,7 @@ CREATE TABLE profesores (
   nombre varchar(45) NOT NULL,  
   email varchar(100) NOT NULL UNIQUE,
   rol varchar(25),
-  id_grupo int
+  id_grupo int,
   FOREIGN KEY (id_grupo) REFERENCES grupo(id_grupo)
 
 );
@@ -55,14 +62,14 @@ CREATE TABLE alumnos (
   id_alumno serial NOT NULL PRIMARY KEY,
   nombre varchar(50) NOT NULL,
   email varchar(100) NOT NULL UNIQUE,
-  id_grupo int
+  id_grupo int,
   FOREIGN KEY (id_grupo) REFERENCES grupo(id_grupo)
 );
 
 -- Crear tabla Proyecto
 CREATE TABLE proyecto (
   id_proyecto serial NOT NULL PRIMARY KEY,
-  nombre_proyecto varchar(50) NOT NULL,
+  nombre_proyecto varchar(50) NOT NULL
  );
 
 -- Crear tabla calificaciones
@@ -70,9 +77,9 @@ CREATE TABLE calificaciones (
   id_calificaciones serial NOT NULL PRIMARY KEY,
   nota varchar(25),
   id_alumno int,
-  id_proyecto int
-  FOREIGN KEY (id_proyecto) REFERENCES proyecto(id_proyecto)
-  FOREIGN KEY (id_alumno) REFERENCES alumno(id_alumno)
+  id_proyecto int,
+  FOREIGN KEY (id_proyecto) REFERENCES proyecto(id_proyecto),
+  FOREIGN KEY (id_alumno) REFERENCES alumnos(id_alumno)
  );
 
 -- Insertar datos en tabla campus
